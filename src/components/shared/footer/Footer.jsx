6a8e909a2c362dcf74598961a67ec5c892066cbb
sheikhzim2004-del <sheikhzim2004@gmail.com>
2026-04-15 18:44:00 +0600

    nav and footer added

--- src/components/shared/footer/Footer.jsx
+++ src/components/shared/footer/Footer.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
+const facebookIcon = new URL('../../../assets/facebook.png', import.meta.url);
+const instagramIcon = new URL('../../../assets/instagram.png', import.meta.url);
+const twitterIcon = new URL('../../../assets/twitter.png', import.meta.url);
+
 
 const Footer = () => {
   return (
-    <div>
-      this is footer
-    </div>
+    <footer className="footer footer-horizontal footer-center bg-[#244D3F] text-primary-content p-10">
+      <aside>
+        <h1 className='text-5xl text-white font-bold'>KeenKeeper</h1>
+        <p>Your personal shelf of meaningful connections. Browse, tend, and nurture the relationships that matter most.</p>
+        
+        <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
+      </aside>
+      <nav className='container mx-auto'>
+        <h3 className="font-bold text-lg">Social Links</h3>
+        <div className="grid grid-flow-col gap-4">
+          <a><img src={instagramIcon} alt="Instagram" /></a>
+          <a><img src={facebookIcon} alt="Facebook" /></a>
+          <a><img src={twitterIcon} alt="Twitter" /></a>
+        </div>
+      </nav>
+      <div>
+          <div className="divider divider-primary"></div>
+          <div className='flex justify-between w-full'>
+            <p>© 2026 KeenKeeper. All rights reserved.</p>
+            <div className='flex gap-2'>
+              <a className="link link-hover">Privacy Policy</a>
+              <a className="link link-hover">Terms of Service</a>
+              <a className="link link-hover">Cookies</a>
+            </div>
+          </div>
+      </div>
+    </footer>
   );
 };
 
--- src/components/shared/navbar/Navbar.jsx
+++ src/components/shared/navbar/Navbar.jsx
@@ -1,46 +1,18 @@
 import React from 'react';
+import { FaHome } from 'react-icons/fa';
+import { ImStatsDots } from 'react-icons/im';
+import { RiTimeLine } from 'react-icons/ri';
 
 const Navbar = () => {
   return (
     <div className="navbar bg-base-100 shadow-sm">
       <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-          </div>
-          <ul
-            tabIndex="-1"
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-            <li><a>Item 1</a></li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
-              </ul>
-            </li>
-            <li><a>Item 3</a></li>
-          </ul>
-        </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li><a>Item 1</a></li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2 bg-base-100 w-40 z-1">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
-              </ul>
-            </details>
-          </li>
-          <li><a>Item 3</a></li>
-        </ul>
+        <a className="btn btn-ghost text-xl"><span className='font-black'>Keen</span>Keeper</a>
       </div>
       <div className="navbar-end">
-        <a className="btn">Button</a>
+        <a className="btn btn-ghost"><FaHome></FaHome> Home</a>
+        <a className="btn btn-ghost"><RiTimeLine /> Timeline</a>
+        <a className="btn btn-ghost"><ImStatsDots /> Stats</a>
       </div>
     </div>
   );
