a431caf32f2a41f2df72fb90aca997fb1b0dd398
sheikhzim2004-del <sheikhzim2004@gmail.com>
2026-04-17 21:40:27 +0600

    rechart added

--- src/pages/stats/Stats.jsx
+++ src/pages/stats/Stats.jsx
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { PieChart, Pie, Tooltip, ResponsiveContainer, Cell } from 'recharts';
+import { FriendContext } from '../../context/FriendContext';
 
 const Stats = () => {
+
+  const { stored } = useContext(FriendContext);
+
+    if (stored.length === 0) {
+    return (
+      <div className="w-full h-`[400px]` flex justify-center items-center">
+        <h2 className="text-xl font-semibold text-gray-500">
+          No activity selected
+        </h2>
+      </div>
+    );
+  }
+
+  const callCount = stored.filter(item => item.type === 'Call').length;
+  const textCount = stored.filter(item => item.type === 'Text').length;
+  const videoCount = stored.filter(item => item.type === 'Video').length;
+
+  const data = [
+    { name: 'Call', value: callCount },
+    { name: 'Text', value: textCount },
+    { name: 'Video', value: videoCount }
+  ];
+
+  // 🎨 colors
+  const COLORS = ['#00C49F', '#FFBB28', '#FF8042'];
+
   return (
-    <div>
-      Stats
+    <div className="w-full `h-[400px]` flex justify-center items-center">
+
+      <ResponsiveContainer width="100%" height="100%">
+        <PieChart>
+          <Pie
+            data={data}
+            dataKey="value"
+            innerRadius={60}
+            outerRadius={100}
+            paddingAngle={5}
+            label
+          >
+            {
+              data.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+              ))
+            }
+          </Pie>
+          <Tooltip />
+        </PieChart>
+      </ResponsiveContainer>
+
     </div>
   );
 };
